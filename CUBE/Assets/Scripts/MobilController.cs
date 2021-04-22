8c93f94ba3594396359891456911d9bc8ea69f6f
egemenkilic <ege.kilic@hotmail.com>
2021-04-22 18:00:25 +0300

    The Cube with C#

---
+++ CUBE/Assets/Scripts/MobilController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobilController : MonoBehaviour
+{
+    public GameObject Player;
+    public Rigidbody rb;
+    public float sidewaysForce = 500f;
+
+    public void Right()
+    {
+        rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    
+    public void Left()
+    {
+        rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+}
